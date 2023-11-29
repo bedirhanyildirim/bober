d455fbfea3bed51ce396eaf8d0669787fe1257e5
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-11-29 06:52:11 +0100

    fix: homepage dummy data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,3 @@
-import {Button} from '~/components/ui/button';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '~/components/ui/dialog';
-import {Mail} from 'lucide-react';
-
 export function meta() {
   return [
     {title: 'bober | bedcodes'},
@@ -21,35 +8,18 @@ export function meta() {
 export default function Index() {
   return (
     <div>
-      <h1 className="font-normal">This is homepage </h1>
-      <Dialog>
-        <DialogTrigger>
-          <Button>
-            <Mail className="mr-2 h-4 w-4" />
-            Click me
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-md h-full md:h-auto">
-          <DialogHeader>
-            <DialogTitle>Natalia look at this</DialogTitle>
-            <DialogDescription>Innit aweseome?</DialogDescription>
-          </DialogHeader>
-          <div className="flex items-center space-x-2">
-            <p className="text-5xl">😜</p>
-          </div>
-          <DialogFooter className="sm:justify-start">
-            <DialogClose asChild>
-              <Button type="button" variant="secondary">
-                Close
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-      <div className="w-full bg-gray-300 h-96 flex items-center">Test 1</div>
-      <div className="w-full bg-gray-400 h-96 flex items-center">Test 2</div>
-      <div className="w-full bg-gray-300 h-96 flex items-center">Test 3</div>
-      <div className="w-full bg-gray-400 h-96 flex items-center">Test 4</div>
+      <div className="w-full h-96 flex justify-center">
+        <div className="content-max-width flex items-center">Section 1</div>
+      </div>
+      <div className="w-full h-96 flex justify-center bg-gray-200">
+        <div className="content-max-width flex items-center">Section 2</div>
+      </div>
+      <div className="w-full h-96 flex justify-center">
+        <div className="content-max-width flex items-center">Section 3</div>
+      </div>
+      <div className="w-full h-96 flex justify-center bg-gray-200">
+        <div className="content-max-width flex items-center">Section 4</div>
+      </div>
     </div>
   );
 }
