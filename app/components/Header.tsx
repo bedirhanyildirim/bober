7026c585d77d873a0e036d3fd0d857112da72f3d
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2023-11-29 06:48:23 +0100

    feat: Header component developed

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,35 +1,26 @@
-import {Await, NavLink} from '@remix-run/react';
-import {Suspense} from 'react';
-import type {HeaderQuery} from 'storefrontapi.generated';
+import {NavLink, useNavigate} from '@remix-run/react';
+import type {HeaderQuery, ShopFragment} from 'storefrontapi.generated';
 import type {LayoutProps} from './Layout';
 import {useRootLoaderData} from '~/root';
 import {Button} from './ui/button';
 import {ShoppingCart} from 'lucide-react';
-import {Badge} from './ui/badge';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/ui/dialog';
 
 type HeaderProps = Pick<LayoutProps, 'header' | 'cart' | 'isLoggedIn'>;
 
-type Viewport = 'desktop' | 'mobile';
-
 export function Header({header, isLoggedIn, cart}: HeaderProps) {
-  const {shop, menu} = header;
   return (
-    <div className="flex justify-center">
-      <header className="content-max-width flex items-center bg-background text-foreground h-[64px] sticky top-0 z-[1]">
-        <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
-          <img
-            src={shop.brand?.logo?.image?.url}
-            alt={shop.name.concat(' logo')}
-            className="h-8"
-          />
-        </NavLink>
-        <HeaderMenu
-          menu={menu}
-          viewport="desktop"
-          primaryDomainUrl={header.shop.primaryDomain.url}
-        />
-        <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
-      </header>
+    <div className="flex justify-center bg-background text-foreground h-[60px] sticky top-0 border-b">
+      <DesktopHeader cart={cart} header={header} isLoggedIn={isLoggedIn} />
+      <MobileHeader cart={cart} header={header} isLoggedIn={isLoggedIn} />
     </div>
   );
 }
@@ -37,54 +28,21 @@ export function Header({header, isLoggedIn, cart}: HeaderProps) {
 export function HeaderMenu({
   menu,
   primaryDomainUrl,
-  viewport,
 }: {
   menu: HeaderProps['header']['menu'];
   primaryDomainUrl: HeaderQuery['shop']['primaryDomain']['url'];
-  viewport: Viewport;
 }) {
-  const {publicStoreDomain} = useRootLoaderData();
-  const className = `header-menu-${viewport}`;
-
-  function closeAside(event: React.MouseEvent<HTMLAnchorElement>) {
-    if (viewport === 'mobile') {
-      event.preventDefault();
-      window.location.href = event.currentTarget.href;
-    }
-  }
-
   return (
-    <nav className={className} role="navigation">
-      {viewport === 'mobile' && (
-        <NavLink
-          className="cursor-pointer transition-colors"
-          end
-          onClick={closeAside}
-          prefetch="intent"
-          style={activeLinkStyle}
-          to="/"
-        >
-          <p className="text-sm text-gray-800 hover:text-primary">Home</p>
-        </NavLink>
-      )}
+    <nav className="hidden md:flex gap-4 ml-12" role="navigation">
       {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
         if (!item.url) return null;
-
-        // if the url is internal, we strip the domain
-        const url =
-          item.url.includes('myshopify.com') ||
-          item.url.includes(publicStoreDomain) ||
-          item.url.includes(primaryDomainUrl)
-            ? new URL(item.url).pathname
-            : item.url;
+        const url = StripURL({primaryDomainUrl, itemURL: item.url});
         return (
           <NavLink
             className="cursor-pointer transition-colors"
             end
             key={item.id}
-            onClick={closeAside}
             prefetch="intent"
-            style={activeLinkStyle}
             to={url}
           >
             <p className="text-sm text-gray-800 hover:text-primary">
@@ -97,76 +55,197 @@ export function HeaderMenu({
   );
 }
 
+function StripURL({
+  primaryDomainUrl,
+  itemURL,
+}: {
+  primaryDomainUrl: HeaderQuery['shop']['primaryDomain']['url'];
+  itemURL: string;
+}) {
+  const {publicStoreDomain} = useRootLoaderData();
+  return itemURL.includes('myshopify.com') ||
+    itemURL.includes(publicStoreDomain) ||
+    itemURL.includes(primaryDomainUrl)
+    ? new URL(itemURL).pathname
+    : itemURL;
+}
+
+function DesktopHeader({header, isLoggedIn, cart}: HeaderProps) {
+  return (
+    <header className="content-max-width hidden md:flex items-center">
+      <NavLink prefetch="intent" to="/" end>
+        <img
+          src={header.shop.brand?.logo?.image?.url}
+          alt={header.shop.name.concat(' logo')}
+          className="h-8"
+        />
+      </NavLink>
+      <HeaderMenu
+        menu={header.menu}
+        primaryDomainUrl={header.shop.primaryDomain.url}
+      />
+      <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+    </header>
+  );
+}
+
 function HeaderCtas({
   isLoggedIn,
   cart,
 }: Pick<HeaderProps, 'isLoggedIn' | 'cart'>) {
+  const navigate = useNavigate();
   return (
-    <nav className="header-ctas" role="navigation">
-      <HeaderMenuMobileToggle />
-      <SearchToggle />
-      <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
-        {isLoggedIn ? (
-          <Button variant="outline" size="sm">
-            Account
-          </Button>
-        ) : (
-          <Button variant="outline" size="sm">
-            Sign in
-          </Button>
-        )}
-      </NavLink>
-      <CartToggle cart={cart} />
+    <nav className="flex items-center gap-4 ml-auto" role="navigation">
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => {
+          navigate('/account');
+        }}
+      >
+        <p className="text-sm text-gray-800 hover:text-primary">
+          {isLoggedIn ? 'Account' : 'Sign in'}
+        </p>
+      </Button>
+      <Button
+        variant="outline"
+        size="smallIcon"
+        onClick={() => {
+          navigate('/cart');
+        }}
+      >
+        <ShoppingCart className="h-4 w-auto" />
+      </Button>
     </nav>
   );
 }
 
-function HeaderMenuMobileToggle() {
+function MobileHeader({header, isLoggedIn, cart}: HeaderProps) {
+  const navigate = useNavigate();
   return (
-    <a className="header-menu-mobile-toggle" href="#mobile-menu-aside">
-      <h3>☰</h3>
-    </a>
-  );
-}
-
-function SearchToggle() {
-  return (
-    <a href="#search-aside">
-      <Button variant="outline" size="sm">
-        Search
-      </Button>
-    </a>
-  );
-}
-
-function CartBadge({count}: {count: number}) {
-  return (
-    <a href="#cart-aside">
-      <Button variant="outline" size="sm" className="relative">
-        <ShoppingCart className="h-4 w-4 block" />
-        {count > 0 ? (
-          <Badge variant="default" className="ml-1">
-            {count}
-          </Badge>
-        ) : (
-          ''
-        )}
-      </Button>
-    </a>
-  );
-}
-
-function CartToggle({cart}: Pick<HeaderProps, 'cart'>) {
-  return (
-    <Suspense fallback={<CartBadge count={0} />}>
-      <Await resolve={cart}>
-        {(cart) => {
-          if (!cart) return <CartBadge count={0} />;
-          return <CartBadge count={cart.totalQuantity || 0} />;
+    <header className="content-max-width flex md:hidden items-center justify-center">
+      <HeaderMenuMobileToggle
+        shop={header.shop}
+        menu={header.menu}
+        primaryDomainUrl={header.shop.primaryDomain.url}
+      />
+      <NavLink prefetch="intent" to="/" end>
+        <img
+          src={header.shop.brand?.logo?.image?.url}
+          alt={header.shop.name.concat(' logo')}
+          className="h-8 w-auto block"
+        />
+      </NavLink>
+      <Button
+        variant="ghost"
+        size="smallIcon"
+        className="ml-auto"
+        onClick={() => {
+          navigate('/cart');
         }}
-      </Await>
-    </Suspense>
+      >
+        <ShoppingCart className="h-5 w-auto block" />
+      </Button>
+    </header>
   );
+}
+
+function HeaderMenuMobileToggle({
+  shop,
+  menu,
+  primaryDomainUrl,
+}: {
+  shop: ShopFragment;
+  menu: HeaderProps['header']['menu'];
+  primaryDomainUrl: HeaderQuery['shop']['primaryDomain']['url'];
+}) {
+  return (
+    <Dialog>
+      <DialogTrigger className="mr-auto">
+        <Button variant="ghost" size="smallIcon">
+          <p className="text-xl">☰</p>
+        </Button>
+      </DialogTrigger>
+      <DialogContent
+        className="w-full h-full max-w-none md:h-fit md:max-w-lg"
+        closeButton="header"
+      >
+        <DialogHeader className="">
+          <DialogTitle className="flex justify-center -mt-3">
+            <NavLink
+              className="cursor-pointer transition-colors"
+              end
+              prefetch="intent"
+              to="/"
+              style={activeLinkStyle}
+            >
+              <img
+                src={shop.brand?.logo?.image?.url}
+                alt={shop.name.concat(' logo')}
+                className="h-8 w-auto block"
+              />
+            </NavLink>
+          </DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col items-center text-center gap-4">
+          <NavLink
+            className="cursor-pointer transition-colors"
+            end
+            prefetch="intent"
+            to="/"
+            style={activeLinkStyle}
+          >
+            <p className="text-gray-800 hover:text-primary">Home</p>
+          </NavLink>
+          <NavLink
+            className="cursor-pointer transition-colors"
+            end
+            prefetch="intent"
+            to="/account"
+            style={activeLinkStyle}
+          >
+            <p className="text-gray-800 hover:text-primary">Account</p>
+          </NavLink>
+          {(menu || FALLBACK_HEADER_MENU).items.map((item) => {
+            if (!item.url) return null;
+            const url = StripURL({primaryDomainUrl, itemURL: item.url});
+            return (
+              <NavLink
+                className="cursor-pointer transition-colors"
+                end
+                key={item.id}
+                prefetch="intent"
+                to={url}
+                style={activeLinkStyle}
+              >
+                <p className="text-gray-800 hover:text-primary">{item.title}</p>
+              </NavLink>
+            );
+          })}
+        </div>
+        <DialogFooter className="sm:flex-col">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+function activeLinkStyle({
+  isActive,
+  isPending,
+}: {
+  isActive: boolean;
+  isPending: boolean;
+}) {
+  return {
+    fontWeight: isActive ? 'bold' : undefined,
+    color: isPending ? 'grey' : 'black',
+  };
 }
 
 const FALLBACK_HEADER_MENU = {
@@ -210,16 +289,3 @@ const FALLBACK_HEADER_MENU = {
     },
   ],
 };
-
-function activeLinkStyle({
-  isActive,
-  isPending,
-}: {
-  isActive: boolean;
-  isPending: boolean;
-}) {
-  return {
-    fontWeight: isActive ? 'bold' : undefined,
-    color: isPending ? 'grey' : 'black',
-  };
-}
